f1eaac97735290af5dc7d7469edbc9ac1e536c21
camilagx <camilac.gomez97@gmail.com>
2023-01-03 14:56:20 -0700

    Day and time is displayed in real time

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,16 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20
-  }
-};
+/*Adding the current day and time to the weather app */
+let dayTimeSpan = document.querySelector('#dayTime');
 
-let city = prompt("Enter a city: ");
-city = city.toLowerCase();
+function getDayName(num){
+  let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-let celciusTemp = Math.round(weather[city].temp);
-let fahrenheitTemp = Math.round((weather[city].temp * 9/5) + 32);
-let humidity = weather[city].humidity;
-
-if(weather[city] !== undefined){
-  alert(`It is currently ${celciusTemp}°C(${fahrenheitTemp}°F) in ${city} with a humidity of ${humidity}`);
-}else{
-  alert(`Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`);
+  return days[num];
 }
+
+let now = new Date();
+let day = getDayName(now.getDay());
+let hour = now.getHours();
+let minutes = now.getMinutes();
+
+dayTimeSpan.innerHTML = `${day} ${hour}:${minutes}`;
+
