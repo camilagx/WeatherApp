f6a945209a38a837c2e7c7eda149834fe78d566a
camilagx <camilac.gomez97@gmail.com>
2023-01-03 20:43:04 -0700

    Convert from fahrenheit to celcius- need update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-/*Adding the current day and time to the weather app */
+//Adding the current day and time to the weather app 
 let dayTimeSpan = document.querySelector('#dayTime');
 
 function getDayName(num){
@@ -14,4 +14,41 @@ let minutes = now.getMinutes();
 
 dayTimeSpan.innerHTML = `${day} ${hour}:${minutes}`;
 
+// Updates heading to the city inputted by user 
+document.querySelector('#cityBtnSearch').addEventListener('click', getCity);
 
+function getCity(){
+  let inputCity = document.querySelector('#citySearchBox').value;
+  let h1City = document.querySelector('#city');
+
+  h1City.innerHTML = inputCity;
+}
+
+// Alternate between celcius to fahrenheit 
+let celcius = 17;
+
+function getFahrFromCelcius(celcius){
+  return (celcius * 9/5) + 32;
+}
+
+//Display temperature in fahrenheit
+document.querySelector('#fahrTemp').addEventListener('onclick',displayFahrenheit());
+
+function displayFahrenheit(){
+  let cityTempSpan = document.querySelector('#cityTemp');
+  cityTempSpan.innerHTML = getFahrFromCelcius(celcius);
+}
+
+//Display temperature in celcius
+document.querySelector('#celciusTemp').addEventListener('onclick',displayCelcius());
+
+function displayCelcius(){
+  let cityTempSpan = document.querySelector('#cityTemp');
+  cityTempSpan.innerHTML = celcius;
+
+}
+
+
+/*
+document.querySelector('#celciusTemp').addEventListener('click',displayCelcius());
+*/
